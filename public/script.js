1b3caea19ad24deb5ae3acb23e8511ac63e2b937
NebilY <112113033+NebilY@users.noreply.github.com>
2023-10-23 03:54:01 +0000

    Brand

--- public/script.js
+++ public/script.js
@@ -42,8 +42,7 @@ function submitData(e) {
   };
 
   // Send the text to the server using fetch API
-
-    // Note - here we can omit the “baseUrl” we needed in Postman and just use a relative path to “/summarize” because we will be calling the API from our Replit!  
+  
   fetch('/summarize', requestOptions)
     .then(response => response.text()) // Response will be summarized text
     .then(summary => {
